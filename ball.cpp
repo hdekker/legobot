39460a779f57c0ca15b2bff395a980492856b3de
hdekker <henk.dekker.73@gmail.com>
2013-11-26 19:19:21 +0100

    Changed IR and US sensors

--- ball.cpp
+++ ball.cpp
@@ -790,7 +790,7 @@ public:
     if (not is_down)
     {
       printf("Throw engine DOWN start\n");
-      int setpoint_angle = (is_half_down) ? -350 : -360;
+      int setpoint_angle = (is_half_down) ? -340 : -350;
       motors_move_to_angle(ROBOT_BALL_THROW_PORT, 30, setpoint_angle, 1);
       printf("Throw engine DOWN done\n");
     }
@@ -810,7 +810,7 @@ public:
     if (not is_half_down)
     {
       printf("Throw engine HALF_DOWN start\n");
-      int setpoint_angle = (is_down) ? 60 : -310;
+      int setpoint_angle = (is_down) ? 60 : -300;
       motors_move_to_angle(ROBOT_BALL_THROW_PORT, 15, setpoint_angle, 1);
       printf("Throw engine HALF_DOWN done\n");
     }
@@ -826,8 +826,9 @@ public:
     down();
     printf("SHOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOT");
     motors_set_speed(ROBOT_BALL_THROW_PORT, -100);
-    sleep_ms(200);
+    sleep_ms(500);
     motors_stop(ROBOT_BALL_THROW_PORT);
+    sleep_ms(100);
     is_up = false;
     is_down = false;
     is_half_down = false;
@@ -887,7 +888,7 @@ public:
     if (is_ok)
     {
       int current_angle = motors_get_angle(ROBOT_BALL_CATCH_PORT);
-      motors_move_to_angle(ROBOT_BALL_CATCH_PORT, 30, -180, 1);
+      motors_move_to_angle(ROBOT_BALL_CATCH_PORT, 80, -180, 1);
       int reached_angle = motors_get_angle(ROBOT_BALL_CATCH_PORT);
       if (abs(reached_angle - current_angle) > 170)
       {
@@ -950,49 +951,66 @@ private:
     time_t currenttime;
     int elapsed = 0;
     int distance = 0;
-    int velocity = -80;
+    int percentage = 0;
+    int velocity = -45;
     int actual_velocity_left = 0;
     int actual_velocity_right = 0;
-    motors_set_speed(ROBOT_WHEEL_RIGHT_PORT, velocity * 11 / 10);
+    motors_set_speed(ROBOT_WHEEL_RIGHT_PORT, velocity);
     motors_set_speed(ROBOT_WHEEL_LEFT_PORT, velocity);
     do
     {
       sleep(1);
       distance = sensors_get_us_distance_mm(ROBOT_ULTRASONIC_SENSOR_PORT);
+      percentage = sensors_get_ir_distance(ROBOT_INFRARED_SENSOR_PORT);
       actual_velocity_right = motors_get_motor_speed(ROBOT_WHEEL_RIGHT_PORT);
       actual_velocity_left = motors_get_motor_speed(ROBOT_WHEEL_LEFT_PORT);
       currenttime = time(NULL);
       elapsed = static_cast<int>(difftime(currenttime, starttime));
-      printf("move_until_collision dist=%d, velocity=%d, left=%d, right=%d, elapsed=%d\n", distance, velocity, actual_velocity_left, actual_velocity_right, elapsed);
+      printf("move_until_collision dist=%d, perc=%d, velocity=%d, left=%d, right=%d, elapsed=%d\n", distance, percentage, velocity, actual_velocity_left, actual_velocity_right, elapsed);
     } while ((elapsed < 30) and /*((distance<120) or (distance>180)) and*/ ((abs(actual_velocity_left)>0) or (abs(actual_velocity_right)>0)));
     motors_stop(ROBOT_WHEEL_RIGHT_PORT);
     motors_stop(ROBOT_WHEEL_LEFT_PORT);
     sleep(1);
     printf("move_until_collision done\n");
   }
+  
+  
+  bool is_target_collision()
+  {
+    
+    // Drive a little bit backward, and turn, with sensors along target
+    move_and_turn(200, -90);
+    int distance = sensors_get_us_distance_mm(ROBOT_ULTRASONIC_SENSOR_PORT);
+    bool is_target = ((distance > 160) and (distance < 240));
+    printf("\n\nis_target_collision=%d, distance=%d\n\n", is_target, distance);
+    //move_and_turn(0, 90);
+    //move_and_turn(-200, 0);
+    return is_target;
+  }
+  
 public:
   void move_until_target()
   {
     printf("move_until_target start\n");
-    int distance = 0;
+    //int distance = 0;
+    //int percentage = 0;
     bool reached_target = false;
     while (not reached_target)
     {
       move_until_collision();
-      distance = sensors_get_us_distance_mm(ROBOT_ULTRASONIC_SENSOR_PORT);
-      if ((distance > 120) and (distance < 180))
+      if (is_target_collision())
       {
         reached_target = true;
       }
       else
       {
-        motors_set_speed(ROBOT_WHEEL_RIGHT_PORT, 60);
-        motors_set_speed(ROBOT_WHEEL_LEFT_PORT, 60);
-        sleep(2);
-        motors_stop(ROBOT_WHEEL_RIGHT_PORT);
-        motors_stop(ROBOT_WHEEL_LEFT_PORT);
+        //motors_set_speed(ROBOT_WHEEL_RIGHT_PORT, 60);
+        //motors_set_speed(ROBOT_WHEEL_LEFT_PORT, 60);
+        //sleep(2);
+        //motors_stop(ROBOT_WHEEL_RIGHT_PORT);
+        //motors_stop(ROBOT_WHEEL_LEFT_PORT);
         sleep(1);
-        command_turn_angle(-135);
+        command_turn_angle(-45);
       }
     }
     printf("move_until_target done\n");
@@ -1078,7 +1096,8 @@ void ball_execute(bool* keep_running)
   while(1)
   {
     wheelhandler->move_until_target();
-    wheelhandler->move_and_turn(-200, 180);
+    wheelhandler->move_and_turn(0, -90);
+    wheelhandler->move_and_turn(100, 0);
     ballhandler->open();
     ballhandler->half_down();
     wheelhandler->move_and_turn(-200, 0);
